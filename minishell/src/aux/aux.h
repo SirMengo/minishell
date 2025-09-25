1f71ceb21c49793d4917247033f2810afd97bb78
xalves <xavierfrpalves2@gmail.com>
2025-09-25 12:00:53 +0100

    added ft_Calloc

--- minishell/src/aux/aux.h
+++ minishell/src/aux/aux.h
@@ -6,7 +6,7 @@
 /*   By: xalves <xalves@student.42lisboa.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/25 11:12:25 by xalves            #+#    #+#             */
-/*   Updated: 2025/09/25 11:27:55 by xalves           ###   ########.fr       */
+/*   Updated: 2025/09/25 12:00:30 by xalves           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,8 @@
 char	**ft_split(char const *s, char c);
 static int	ft_count_words(char const *s, char sep);
 
+// ft_calloc
+void	*ft_calloc(size_t nmemb, size_t size);
 
 
 #endif
---
+++ minishell/src/aux/ft_calloc.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: xalves <xalves@student.42lisboa.com>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/25 11:47:12 by xalves            #+#    #+#             */
+/*   Updated: 2025/09/25 11:47:57 by xalves           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+# include "aux.h";
+
+
+void	*ft_calloc(size_t nmemb, size_t size)
+{
+	void			*ptr;
+	size_t			m;
+	size_t			i;
+	unsigned char	*ptr_aux;
+
+	m = nmemb * size;
+	if (nmemb == 0 || size == 0)
+		return (malloc(0));
+	if (nmemb > (m / size))
+		return (NULL);
+	ptr = malloc(nmemb * size);
+	if (ptr == NULL)
+		return (NULL);
+	i = 0;
+	ptr_aux = (unsigned char *)ptr;
+	while (i < m)
+	{
+		ptr_aux[i] = 0;
+		i++;
+	}
+	return (ptr_aux);
+}
